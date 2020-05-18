7f748bafc97b9580f4f53237bf8d30f7a80e6abb
sgxingh <65164707+sgxingh@users.noreply.github.com>
2020-05-18 16:51:09 +0530

    update

--- analyzing_covid_cases_south_korea/covid_cases.py
+++ analyzing_covid_cases_south_korea/covid_cases.py
@@ -7,9 +7,19 @@ import numpy as np
 data = pd.ExcelFile("TimeAge.xls")
 
 # clean up data to use for further analysis
-data_date = data.parse(u'TimeAge')
+# parse data to use only one days data 2020-03-02
+data_date = data.parse(u'TimeAge', skipfooter=655)
+#print(data_date)
 
 # drop any empty rows just to take precaution as it is a larger dataset
 data_date.dropna(inplace=True)
 
-# change index to date 
+# print data
+print("after clean up:")
+print(data_date)
+
+# plot data - age with confirmed and deceased through all data 
+data_date['confirmed'].plot(label="Confirmed")
+data_date['deceased'].plot(label="Deceased")
+plt.legend(loc="upper right")
+plt.show()
