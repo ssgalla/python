6cff00fe3bbf9b4202804ec1b4893412346d6ad4
sgxingh <65164707+sgxingh@users.noreply.github.com>
2020-05-19 12:52:33 +0530

    -

--- analyzing_covid_cases_south_korea/analyzing_case_data_city/covid_cases.py
+++ analyzing_covid_cases_south_korea/analyzing_case_data_city/covid_cases.py
@@ -7,8 +7,31 @@ import numpy as np
 df = pd.read_csv("Case.csv")
 
 # clean up data to use for further analysis
+#
 # drop columns with insufficient data
 df.drop(df[df['city'] == "-"].index, inplace=True)
 df.drop(df[df['longitude'] == "-"].index, inplace=True)
-#print(df.head(50))
 
+# drop information not required for analysis
+df.drop(['group'], axis=1, inplace=True)
+print(df.head(80))
+
+# slicing data: Seoul records
+df_seoul = df[:8]
+#print("seoul clean data:")
+#print(df_seoul)
+
+# slicing data: Busan
+df_busan = df[8:12]
+#print("busan clean data:")
+#print(df_busan)
+
+# slicing data: Daegu
+df_daegu = df[12:17]
+#print("daegu clean data:")
+#print(df_daegu)
+
+# slicing data: Daejeon
+df_daejeon = df.iloc[17]
+#print("daejeon clean data:")
+#print(df_daejeon)
