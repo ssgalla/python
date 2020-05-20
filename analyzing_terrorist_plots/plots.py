3e348f772853117ded5d7d7d499fe96a45c8c06b
sgxingh <65164707+sgxingh@users.noreply.github.com>
2020-05-20 21:11:50 +0530

    -

--- analyzing_terrorist_plots/plots.py
+++ analyzing_terrorist_plots/plots.py
@@ -12,21 +12,24 @@ data_plotstatus = df
 ##### How many terrorist attacks were executed in comparison to how many were prevented? #####
 # data clean up required for this analysis 
 # remove records where no details are recorded on whether attacks was prevented or not prevented
-data_plotstatus.dropna(subset = ["plot_status"], inplace=True)
+data_plotstatus.dropna(subset = ["plot_status"])
 
 # drop unnecessary information for this question
-data_plotstatus.drop(['plot_ID'], axis=1, inplace=True)
-data_plotstatus.drop(['plot_ideology'], axis=1, inplace=True)
-data_plotstatus.drop(['prevention_method'], axis=1, inplace=True)
-data_plotstatus.drop(['attack_date'], axis=1, inplace=True)
-data_plotstatus.drop(['victims_wounded'], axis=1, inplace=True)
-data_plotstatus.drop(['victims_killed'], axis=1, inplace=True)
-data_plotstatus.drop(['plot_name'], axis=1, inplace=True)
+data_plotstatus.drop(['plot_ID'], axis=1)
+data_plotstatus.drop(['plot_ideology'], axis=1)
+data_plotstatus.drop(['prevention_method'], axis=1)
+data_plotstatus.drop(['attack_date'], axis=1)
+data_plotstatus.drop(['victims_wounded'], axis=1)
+data_plotstatus.drop(['victims_killed'], axis=1)
+data_plotstatus.drop(['plot_name'], axis=1)
 
 # find how many plots were prevented
-prevented_plots = data_plotstatus[data_plotstatus['plot_status'].str.match('Prevented')]
-notprevented_plots = data_plotstatus[data_plotstatus['plot_status'].str.match('NotPrevented')]
+prevented_plots = data_plotstatus[data_plotstatus['plot_status'] == "Prevented"]
+prevented_plots = len(prevented_plots)
 
-prevented_total = prevented_plots.len()
+# find how many plots were not prevented
+notprevented_plots = data_plotstatus[data_plotstatus['plot_status'] == 'NotPrevented']
+notprevented_plots = len(notprevented_plots)
 
-print(prevented_total)
+print(prevented_plots)
+print(notprevented_plots)
