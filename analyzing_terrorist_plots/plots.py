3885b2770ff5f2a6ea1ab90f9785fd10d1d5f141
sgxingh <65164707+sgxingh@users.noreply.github.com>
2020-05-21 17:33:12 +0530

    -

--- analyzing_terrorist_plots/plots.py
+++ analyzing_terrorist_plots/plots.py
@@ -6,11 +6,10 @@ import numpy as np
 ##### read csv file containing data #####
 df = pd.read_csv("Plots.csv")
 
-##### create seperate variable for first part of analysis
+##### create seperate variable for first part of analysis #####
 data_plotstatus = df
 
 ##### How many terrorist attacks were executed in comparison to how many were prevented? #####
-# data clean up required for this analysis 
 # remove records where no details are recorded on whether attacks was prevented or not prevented
 data_plotstatus.dropna(subset = ["plot_status"])
 
@@ -24,12 +23,21 @@ data_plotstatus.drop(['victims_killed'], axis=1)
 data_plotstatus.drop(['plot_name'], axis=1)
 
 # find how many plots were prevented
-prevented_plots = data_plotstatus[data_plotstatus['plot_status'] == "Prevented"]
-prevented_plots = len(prevented_plots)
+prevented_plots = len(data_plotstatus[data_plotstatus['plot_status'] == "Prevented"])
 
 # find how many plots were not prevented
-notprevented_plots = data_plotstatus[data_plotstatus['plot_status'] == 'NotPrevented']
-notprevented_plots = len(notprevented_plots)
+notprevented_plots = len(data_plotstatus[data_plotstatus['plot_status'] == 'NotPrevented'])
 
-print(prevented_plots)
-print(notprevented_plots)
+# Data to plot
+labels = 'Prevented', 'Not Prevented'
+sizes = [prevented_plots, notprevented_plots]
+colors = ['lightgreen', 'lightcoral']
+explode = (0.2, 0)  # explode 1st slice
+
+# Plot
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.axis('equal')
+plt.title("Percentage of terrorist attacks prevented between 2001-2016\n" + "compared to that of attacks not prevented")
+plt.show()
